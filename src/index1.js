4596fd56e181945d06f7515026f9b2dd7e0ac5e9
csmSimona <980715844@qq.com>
2019-12-16 21:07:13 +0800

    update 工厂模式

---
+++ src/index1.js
@@ -0,0 +1,27 @@
+// 工厂模式
+class Product {
+    constructor(name) {
+        this.name = name
+    }
+    init() {
+        alert('init')
+    }
+    fn1() {
+        alert('this is fn1')
+    }
+    fn2() {
+        alert('this is fn2')
+    }
+}
+class Creator {
+    create(name) {
+        return new Product(name)
+    }
+}
+
+//测试
+let creator = new Creator()
+let p = creator.create('p1')
+p.init()
+p.fn1()
+p.fn2()
